4e45b9df10dde86b9e980def8abae07652c6d94a
JesusEnjamio <jenjamiob@gmail.com>
2025-03-18 16:32:56 +0100

    login done

---
+++ src/main/java/com/example/reciclArte_backend/service/UserService.java
@@ -0,0 +1,33 @@
+package com.example.reciclArte_backend.service;
+
+import com.example.reciclArte_backend.entity.User;
+import com.example.reciclArte_backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        userRepository.findByEmail(user.getEmail())
+            .ifPresent(existing -> {
+                throw new RuntimeException("El usuario ya está registrado");
+            });
+        return userRepository.save(user);
+    }
+
+    public Optional<User> loginUser(String email, String password) {
+        return userRepository.findByEmail(email)
+                .filter(user -> user.getPassword().equals(password));
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
