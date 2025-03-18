98af692036694883eaf1952973973da90df1a705
Paola Perdomo <paolaperdomo079@gmail.com>
2025-03-18 11:15:26 +0100

    first commit

---
+++ src/main/java/com/example/reciclArte_backend/ReciclArteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.reciclArte_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclArteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclArteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reciclArte_backend/ReciclArteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reciclArte_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclArteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
